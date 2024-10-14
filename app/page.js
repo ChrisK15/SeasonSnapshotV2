dee5271f885cba653ab43e61b682095f6241599c
Mustafar <chris.kelamyan.115@gmail.com>
2024-10-13 19:45:31 -0700

    added imports

--- app/page.js
+++ app/page.js
@@ -1,25 +1,35 @@
 'use client';
 import {
+  Box,
   Button,
-
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableSortLabel,
   Typography,
   Grid,
-} from "@mui/material";
-import Link from "next/link";
-import React from "react";
+} from '@mui/material';
+import Link from 'next/link';
+import React from 'react';
 
 export default function Home() {
   return (
     <div
       style={{
         backgroundImage: "url('/images/home-background.jpg')",
-        backgroundSize: "cover", // Ensures the image covers the whole screen
-        backgroundPosition: "center", // Centers the image
-        backgroundRepeat: "no-repeat", // Prevents image from repeating
-        minHeight: "100vh", // Full screen height
-        display: "flex", // Use flexbox for centering
-        justifyContent: "center", // Center horizontally
-        alignItems: "center", // Center vertically
+        backgroundSize: 'cover', // Ensures the image covers the whole screen
+        backgroundPosition: 'center', // Centers the image
+        backgroundRepeat: 'no-repeat', // Prevents image from repeating
+        minHeight: '100vh', // Full screen height
+        display: 'flex', // Use flexbox for centering
+        justifyContent: 'center', // Center horizontally
+        alignItems: 'center', // Center vertically
       }}
     >
       <Grid
@@ -28,17 +38,20 @@ export default function Home() {
         direction="column"
         alignItems="center"
         justifyContent="center"
-        style={{ textAlign: "center" }}
+        style={{ textAlign: 'center' }}
       >
         {/* Title */}
         <Grid item xs={12}>
-          <Typography variant="h1" style={{ color: "white" }}>
+          <Typography variant="h1" style={{ color: 'white' }}>
             Season
           </Typography>
-          <Typography variant="h1" style={{ color: "white" }}>
+          <Typography variant="h1" style={{ color: 'white' }}>
             Snapshot
           </Typography>
-          <Typography variant="h3" style={{ color: "white", marginTop: "20px" }}>
+          <Typography
+            variant="h3"
+            style={{ color: 'white', marginTop: '20px' }}
+          >
             Every stat, every game, in a snap.
           </Typography>
         </Grid>
