abf9fcde8d273bdfbf16b6a0f69896acd96d771c
ChrisK15 <chris.kelamyan.115@gmail.com>
2024-10-22 22:28:29 -0700

    removed standingsData and implemented into TeamData

--- app/hooks/usePlayerData.js
+++ app/hooks/usePlayerData.js
@@ -1,33 +1,35 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const usePlayerData = (teamID, year) => {
-    const [playerStats, setPlayerStats] = useState([]);
-    const [loading, setLoading] = useState(false);
+  const [playerStats, setPlayerStats] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchPlayerData = async () => {
-            setLoading(true);
-            try {
-                const response = await axios.post('/api/proxy/teamStats', {
-                    teamID: teamID,
-                    year: year,
-                });
-                const { players } = response.data;
-                setPlayerStats(players);
-            } catch (error) {
-                console.error('Error fetching player stats:', error.message);
-            } finally {
-                setLoading(false);
-            }
-        };
+  useEffect(() => {
+    const fetchPlayerData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.post('/api/proxy/teamStats', {
+          teamID: teamID,
+          year: year,
+        });
+        const { players } = response.data;
+        setPlayerStats(players);
+      } catch (error) {
+        console.error('Error fetching player stats:', error.message);
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        if (teamID && year) {
-            fetchPlayerData();
-        }
-    }, [teamID, year]);
+    if (teamID && year) {
+      fetchPlayerData();
+    }
+  }, [teamID, year]);
 
-    return { playerStats, loading };
+  return { playerStats, loading, error };
 };
 
 export default usePlayerData;
--- app/hooks/useTeamData.js
+++ app/hooks/useTeamData.js
@@ -1,33 +1,66 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const useTeamData = (teamID, year) => {
-    const [teamStats, setTeamStats] = useState([]);
-    const [loading, setLoading] = useState(false);
+  const [teamStats, setTeamStats] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchTeamData = async () => {
-          setLoading(true);
-          try {
-            const response = await axios.post('/api/proxy/teamStats', {
-              teamID: teamID,
-              year: year,
-            });
-            const { teamStats } = response.data;
-            setTeamStats([teamStats]);
-          } catch (error) {
-            console.error('Error fetching team stats:', error.message);
-          } finally {
-            setLoading(false);
-          }
-        };
-    
-        if (teamID && year) {
-          fetchTeamData();
+  useEffect(() => {
+    const fetchTeamData = async () => {
+      setLoading(true);
+      try {
+        const teamStatsResponse = await axios.post('/api/proxy/teamStats', {
+          teamID: teamID,
+          year: year,
+        });
+
+        const { teamStats } = teamStatsResponse.data;
+
+        const standingsResponse = await axios.post('/api/proxy/teamStandings', {
+          year: year,
+        });
+
+        const standings = standingsResponse.data.conferences
+          .flatMap((conference) =>
+            conference.divisions.flatMap((division) => division.teams)
+          )
+          .find((team) => team.id === teamID);
+
+        if (standings) {
+          const standingsData = {
+            games_played: standings.wins + standings.losses,
+            wins: standings.wins,
+            losses: standings.losses,
+            win_percentage: (
+              standings.wins /
+              (standings.wins + standings.losses)
+            ).toFixed(3),
+          };
+
+          const updatedTeamStats = {
+            ...teamStats,
+            ...standingsData,
+          };
+
+          setTeamStats([updatedTeamStats]);
+        } else {
+          setTeamStats([teamStats]);
         }
-      }, [teamID, year]);
-    
-      return { teamStats, loading };
+      } catch (error) {
+        console.error('Error fetching team data:', error.message);
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
     };
 
-    export default useTeamData;
+    if (teamID && year) {
+      fetchTeamData();
+    }
+  }, [teamID, year]);
+
+  return { teamStats, loading, error };
+};
+
+export default useTeamData;
