dc7ff102aa692c0a59d2467ed3210e5d860c3104
EsaiArias <esai.arias-peinado.484@my.csun.edu>
2024-10-25 16:55:50 -0700

    Refresh Git cache to apply .gitignore changes

--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
 'use client';
 import { Button, Typography, Grid } from '@mui/material';
 import Link from 'next/link';
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Home() {
+  // State to manage the visibility of the league buttons
+  const [showCategories, setShowCategories] = useState(false);
+
+  // Function to show the league buttons when "Get Started" is clicked
+  const handleGetStartedClick = () => {
+    setShowCategories(true);
+  };
+
   return (
     <div
       style={{
@@ -41,14 +49,61 @@ export default function Home() {
           </Typography>
         </Grid>
 
-        {/* Button */}
+        {/* Get Started Button */}
         <Grid item xs={12}>
-          <Link href="/pages/nba_page">
-            <Button variant="contained" size="large">
-              Get Started
-            </Button>
-          </Link>
+          <Button
+            variant="contained"
+            size="large"
+            onClick={handleGetStartedClick}
+          >
+            Get Started
+          </Button>
         </Grid>
+
+        {/* League Buttons (appear after Get Started is clicked) */}
+        {showCategories && (
+          <Grid
+            container
+            item
+            xs={12}
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
+          >
+            {/* NBA Button */}
+            <Grid item>
+              <Link href="/pages/nba_page" passHref>
+                <Button 
+                  variant="contained" 
+                  size="large" 
+                  color="error"
+                  startIcon={
+                    <img
+                      src='/images/NBA_logo.jpg'
+                      alt= "NBA Logo"
+                      style={{width: '24px', height: '24px'}}
+                      />
+                  }>
+                  NBA
+                </Button>
+              </Link>
+            </Grid>
+
+            {/* MLB Button */}
+            <Grid item>
+              <Button variant="contained" size="large" color="success">
+                MLB
+              </Button>
+            </Grid>
+
+            {/* NFL Button */}
+            <Grid item>
+              <Button variant="contained" size="large" color="warning">
+                NFL
+              </Button>
+            </Grid>
+          </Grid>
+        )}
       </Grid>
     </div>
   );
